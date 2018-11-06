58b9c23b649c24552f4b34bdf6aa1f5148f0ce11
jinoohome <jinoohome@naver.com>
2018-11-06 13:22:53 +0900

    # WARNING: head commit changed in the meantime
    tes

--- src/main/java/com/jeanbro/webservice/web/WebController.java
+++ src/main/java/com/jeanbro/webservice/web/WebController.java
@@ -17,6 +17,7 @@ public class WebController {
     @GetMapping("/")
     public String main(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
-        return "main";
+        return "index.html";
+        /*	return "main";*/
     }
 }
