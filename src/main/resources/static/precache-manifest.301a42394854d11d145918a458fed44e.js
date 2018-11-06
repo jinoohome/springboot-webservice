da17c96ad79be6f3c7a1111b9a34dd8c2d3c2791
jinoohome <jinoohome@gmail.com>
2018-11-06 13:23:03 +0900

    test

---
+++ src/main/resources/static/precache-manifest.301a42394854d11d145918a458fed44e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e43b75a6ca085c20b430",
+    "url": "./js/main.e43b75a6.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "./js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "f5f0410f04b4b2e2451c437d4fe70101",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e43b75a6ca085c20b430",
+    "url": "./css/main.b3159c3c.chunk.css"
+  }
+];
